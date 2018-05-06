fa5c6bd5470caa67a27d48b6d7cfbc391f641196
Kamen <k.todorov@ultraplay.co>
2018-05-06 11:14:52 +0300

    introduced protocol for packing; sepatate the unpack per atom

--- lib/msgpack/msgpacker.ex
+++ lib/msgpack/msgpacker.ex
@@ -1,7 +1,11 @@
-defmodule Msgpacker do
+defprotocol Msgpacker do
+  @doc "Pack elixir obejct to msgpack value"
+  def pack(item)
+end
+
+defimpl Msgpacker, for: Atom do
   def pack(nil) do [0xc0] end
 
   def pack(false) do [0xc2] end
   def pack(true) do [0xc3] end
-
 end
--- lib/msgpack/msgunpacker.ex
+++ lib/msgpack/msgunpacker.ex
@@ -1,10 +1,8 @@
 defmodule Msgunpacker do
+
+  @doc "Unpack msgpacked atom"
   def unpack([0xc0]), do: nil
 
   def unpack([0xc2]), do: false
   def unpack([0xc3]), do: true
-
-  def unpack(_) do
-    throw "not implemented"
-  end
 end
